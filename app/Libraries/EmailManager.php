62152d005877f6f23c379973210cce7a0e416ac3
Your Name <you@example.com>
2025-09-05 14:37:13 +0200

    email manager

--- app/Libraries/EmailManager.php
+++ app/Libraries/EmailManager.php
@@ -7,23 +7,38 @@ class EmailManager
     {
         $email = service('email');
 
-        // Email configuration
+        // Email configuration TEST
+        // $config = [
+        //     'protocol' => 'smtp',
+        //     'SMTPHost' => 'smtp.office365.com',
+        //     'SMTPUser' => 'app-noreply@etiqube.com',
+        //     'SMTPPass' => 'Daz72614',
+        //     'SMTPPort' => '587',
+        //     'SMTPCrypto' => 'tls',
+        //     'mailType' => 'html',
+        //     'charset' => 'utf-8',
+        //     'wordWrap' => true,
+        // ];
+
+        // Email configuration PROD
         $config = [
             'protocol' => 'smtp',
-            'SMTPHost' => 'smtp.office365.com',
-            'SMTPUser' => 'app-noreply@etiqube.com',
-            'SMTPPass' => 'Daz72614',
-            'SMTPPort' => '587',
+            'SMTPHost' => 'mail.crmaurim.it',
+            'SMTPUser' => 'noreply@crmaurim.it',
+            'SMTPPass' => '5BQSJkcYiG#]',
+            'SMTPPort' => '465',
             'SMTPCrypto' => 'tls',
             'mailType' => 'html',
             'charset' => 'utf-8',
             'wordWrap' => true,
         ];
+        
 
         $email->initialize($config);
 
         // Set email parameters
-        $email->setFrom('app-noreply@etiqube.com', 'AURIM');
+        //$email->setFrom('app-noreply@etiqube.com', 'AURIM');
+        $email->setFrom('noreply@crmaurim.it', 'AURIM');
         $email->setTo($to);
 
         if(!empty($cc))
